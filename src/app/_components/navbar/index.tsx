912ea8a60e8dfdd248455af609a6c5064ae3effc
JP Higgins <97922344+jphiggzz@users.noreply.github.com>
2025-06-10 13:20:05 -0400

    add sidebar toggle to navbar on desktop

--- src/app/_components/navbar/index.tsx
+++ src/app/_components/navbar/index.tsx
@@ -1,16 +1,20 @@
 import { Logo } from "@/components/ui/logo";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 import { AccountButton } from "./account-button";
 import { ColorModeToggle } from "./color-mode-toggle";
 
 export const Navbar = () => {
   return (
-    <div className="flex items-center justify-between p-2 md:justify-end">
+    <div className="flex items-center justify-between p-2">
       <div className="flex items-center gap-2 md:hidden">
         <Logo className="size-6" />
         <h1 className="overflow-hidden text-2xl font-bold whitespace-nowrap">
           Open Chat
         </h1>
+      </div>
+      <div className="hidden md:block">
+        <SidebarTrigger />
       </div>
       <div className="flex items-center gap-2">
         <AccountButton />
